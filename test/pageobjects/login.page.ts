32f15d62ce87d8d0f7ccde75ac89273d03412423
subrat08032000 <subrat@sourceintegreta.com>
2025-10-31 00:53:56 +0530

    changes logout and code update

--- test/pageobjects/login.page.ts
+++ test/pageobjects/login.page.ts
@@ -13,6 +13,9 @@ class LoginPage extends Page {
     public get managerRole() {
         return $(`//option[@value="MANAGER"]`)
     }
+    public get AdminRole() {
+        return $(`//option[@value="ADMIN"]`)
+    }
     public get userName() {
         return $(`//input[@id="email"]`);
     }
@@ -29,13 +32,25 @@ class LoginPage extends Page {
     public getRole(roleName: string) {
         return $(`//span[text()="${roleName}"]`);
     }
+    public get Profilebutton_Manager(){
+        return $(`//span[text()='SU']`)
+    }
+     public get Profilebutton_Admin(){
+        return $(`//span[text()='AI']`)
+    }
+
+    public get LogoutBtn(){
+        return $(`//span[text()='Logout']`)
+    }
+    public get SigninToAIPHeader(){
+        return $("//h3[text()='Sign in to AIP Dashboard']")
+    }
 
     /**
      * Author: Subrat
-     * Date: 21-10-2025
      * TC-Login As Manager
      */
-    public async login(username: string, password: string) {
+    public async login_Manager(username: string, password: string) {
         await this.managerRole.click()
         await this.userName.setValue(username);
         await this.loginPassword.setValue(password);
@@ -44,7 +59,28 @@ class LoginPage extends Page {
         await expect(this.Dashboardtxt).toHaveText('Dashboard');
         await expect(this.getRole("MANAGER")).toHaveText("MANAGER");
     }
+public async login_Admin(username: string, password: string) {
+        await this.AdminRole.click()
+        await this.userName.setValue(username);
+        await this.loginPassword.setValue(password);
+        await this.ContinueBTN.click();
+        await this.Dashboardtxt.isDisplayed();
+        await expect(this.Dashboardtxt).toHaveText('Dashboard');
+        await expect(this.getRole("ADMIN")).toHaveText("ADMIN");
+    }
 
+
+public async logout(role: "Manager" | "Admin" | "User"): Promise<void> {
+    const profileButton = role === "Manager" ? this.Profilebutton_Manager : this.Profilebutton_Admin;
+
+    await profileButton.click();
+    await this.LogoutBtn.click();
+    await this.SigninToAIPHeader.isElementDisplayed("Sign in to AIP DashboardSign in to AIP Dashboard")
+    }
 }
+
+
+
+
 
 export default new LoginPage();
--- test/specs/LoginPage_test.e2e.ts
+++ test/specs/LoginPage_test.e2e.ts
@@ -1,14 +1,23 @@
 // import { expect } from '@wdio/globals'
+import loginPage from '../pageobjects/login.page.js';
 import LoginPage from '../pageobjects/login.page.js'
 // import SecurePage from '../pageobjects/secure.page.js'
 import testdata from '../pageobjects/testdata.json';
 
 describe('[TS_Login_02] Verify the Login page', () => {
     
-    it('should login with valid credentials', async () => {
+    it('Manager should able to login with valid credentials', async () => {
         await LoginPage.open(testdata.URL);
         await browser.maximizeWindow();
-        await LoginPage.login(testdata.manager_username,testdata.Manager_password);
+        await LoginPage.login_Manager(testdata.manager_username,testdata.Manager_password);
+        await loginPage.logout('Manager')
+    })
+
+    it('Admin should able to login with valid credentials', async () => {
+        await LoginPage.open(testdata.URL);
+        await browser.maximizeWindow();
+        await LoginPage.login_Admin(testdata.Admin_Username,testdata.Admin_Password);
+        await loginPage.logout('Admin')
     })
 })
 
